6bdee83cbcf015a1bdf3017bc5acfb41ae189d27
Muhammed Rashid <rashidktr07@gmail.com>
2023-02-15 17:07:26 +0530

    Complete Starting page Design

--- src/Pages/StartingPage.js
+++ src/Pages/StartingPage.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import Logo from '../Images/quiz.png'
 
 function StartingPage() {
   return (
     <div className='bw-full h-screen bg-[rgba(12,18,33)]'>      
-      <div className="container">
-        <div className="logo">
-          <img src="" alt="" />
+      <div className="container w-full h-full mx-auto flex flex-col justify-center items-center">
+        <div className="logo  h-1/2 w-1/2 flex justify-center items-end my-5">
+          <div className='h-1/2  rounded-3xl p-10 relative'>
+            <div className='w-full h-full bg-[rgba(255,255,255,0.12)] rounded-full animate-ping absolute top-0 left-0'></div>            
+            <img className='object-cover h-full z-50 ' src={Logo} alt="" />
+          </div>
         </div>
-        <div className="heading">
-          <h1>QUIZZ</h1>
+        <div className="heading h-1/2 w-1/2 flex justify-center items-center pl-5 text-center">
+          <h1 className='text-[rgba(128,142,162)]  text-[.5rem] md:text-[.7rem] lg:text-[.6rem] xl:text-[.7rem] font-montserrat'>To learn you have to listen, <br /> to improve you have to try</h1>
         </div>
       </div>
     </div>
